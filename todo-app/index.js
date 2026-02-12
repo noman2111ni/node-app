fdb07d9a4695bf16ab6d5b4f6b8b232643d2c282
noman <nomanshabeer27@gmail.com>
2026-02-12 12:18:51 +0500

    node-tot-app

---
+++ todo-app/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+let todos = [];
+let id = 1;
+
+// Home Page
+app.get("/", (req, res) => {
+  res.render("index", { todos });
+});
+
+// Add Todo
+app.post("/add", (req, res) => {
+  const newTodo = {
+    id: id++,
+    task: req.body.task,
+  };
+  todos.push(newTodo);
+  res.redirect("/");
+});
+
+// Delete Todo
+app.post("/delete/:id", (req, res) => {
+  const todoId = parseInt(req.params.id);
+  todos = todos.filter((todo) => todo.id !== todoId);
+  res.redirect("/");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
